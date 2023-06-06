79ffa7ebeb97e80eee9f18766309eeb1d6575beb
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2023-06-06 10:06:29 +0800

    latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{html,js}",
-      "./node_modules/tw-elements/dist/js/**/*.js"
-    ],
-    plugins: [require("tw-elements/dist/plugin.cjs")],
-    darkMode: "class"
-  };
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
