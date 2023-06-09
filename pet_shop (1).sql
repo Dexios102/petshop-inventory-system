b917da754eef0f73443386a4faca5d043b29ac56
SugarBab <130421493+SugarBab@users.noreply.github.com>
2023-06-09 17:43:22 +0800

    Add files via upload

---
+++ pet_shop (1).sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 09, 2023 at 11:42 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pet_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_inventory`
+--
+
+CREATE TABLE `tbl_inventory` (
+  `inv_id` int(11) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `img` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_inventory`
+--
+
+INSERT INTO `tbl_inventory` (`inv_id`, `product_name`, `description`, `quantity`, `price`, `img`) VALUES
+(1, 'Taguan ng aso', 'Putang inang aso yan ang baho', 19, 2000, 'Screenshot (101).png'),
+(2, 'Higaan ng aso', 'Galisin ang aso mo', 26, 3000, 'Screenshot (100).png'),
+(3, 'Upuan', 'sadsad', 37, 2000, 'Screenshot (102).png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_orders`
+--
+
+CREATE TABLE `tbl_orders` (
+  `order_id` int(11) NOT NULL,
+  `inv_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `order_quantity` int(11) NOT NULL,
+  `price_perunit` int(11) NOT NULL,
+  `total_price` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_orders`
+--
+
+INSERT INTO `tbl_orders` (`order_id`, `inv_id`, `name`, `email`, `product_name`, `order_quantity`, `price_perunit`, `total_price`, `status`) VALUES
+(21, 1, 'sadasdasdsf', 'test@gmail.com', 'Taguan ng aso', 4, 2000, 8000, 'Approved'),
+(22, 3, 'sadasdasdsf', 'test@gmail.com', 'Upuan', 3, 2000, 6000, 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `id` int(11) NOT NULL,
+  `priv` int(11) NOT NULL,
+  `fullname` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `priv`, `fullname`, `email`, `password`) VALUES
+(3, 1, 'Goerge Sulongkawayan', 'sulong@gmail.com', 'qwert'),
+(4, 1, 'asdasd', 'sad@gmail.com', 'qwewqe'),
+(5, 1, 'sadasdasdsf', 'test@gmail.com', 'qwert'),
+(6, 1, 'asdsadg', 'testmail@gmail.com', 'qwert'),
+(7, 1, 'testname', 'name@gmail.com', 'sadasdasd'),
+(9, 2, 'Petshop Admin', 'petshop_admin@pet.com', 'petshop_admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_inventory`
+--
+ALTER TABLE `tbl_inventory`
+  ADD PRIMARY KEY (`inv_id`);
+
+--
+-- Indexes for table `tbl_orders`
+--
+ALTER TABLE `tbl_orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_inventory`
+--
+ALTER TABLE `tbl_inventory`
+  MODIFY `inv_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbl_orders`
+--
+ALTER TABLE `tbl_orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
